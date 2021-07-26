e2cdf0d64b7c3bb5d2791e240fbfcb50cdba5fb9
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-07-26 15:30:21 -0400

    changed assertArraysEqual to use eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,13 @@
 const assertArraysEqual = function(arry1, arry2) {
+  if (eqArrays(arry1, arry2) === true) {
+    return '✅✅✅ ' + 'Assertion Passed: Your Arrays are equal!';
+  
+  } else if (eqArrays(arry1, arry2) === false) {
+    return '❌❌❌ ' + 'Assertion Failed: Your Arrays are not equal!';
+  }
+};
+
+const eqArrays = function(arry1, arry2) {
   const eqValues = [];
   if (arry1.length === arry2.length) {
     for (let i = 0; i < arry1.length; i++) {
@@ -8,10 +17,9 @@ const assertArraysEqual = function(arry1, arry2) {
     }
   }
   if (eqValues.length === arry1.length) {
-    return '✅✅✅ ' + 'Assertion Passed: Your Arrays are equal!';
-  
+    return true;
   } else {
-    return '❌❌❌ ' + 'Assertion Failed: Your Arrays are not equal!';
+    return false;
   }
 };
 
