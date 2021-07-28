25113044f77e025f5843340c429a57f5a803ce4e
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-07-27 22:21:47 -0400

    used stringify to compare objects

--- countLetters.js
+++ countLetters.js
@@ -24,17 +24,17 @@ const countLetters = function(sentence) {
 
 console.log(countLetters("lighthouse in the house"));
 
-// assertEqaul will not funtion here, since the function cannot compare 2 objects
+// assertEqaul will not funtion here, since the function cannot compare 2 objects thus objests have to be stringified
 
-// assertEqual(countLetters("lighthouse in the house"), {
-//   l: 1,
-//   i: 2,
-//   g: 1,
-//   h: 4,
-//   t: 2,
-//   o: 2,
-//   u: 2,
-//   s: 2,
-//   e: 3,
-//   n: 1,
-// });
+assertEqual(JSON.stringify(countLetters("lighthouse in the house")), JSON.stringify({
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+}));
