ff9f67981ef4b078b380d9365c693491d0ffa70f
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-07 14:26:24 -0400

    remove test code and export function to modules

--- middle.js
+++ middle.js
@@ -1,28 +1,3 @@
-const eqArrays = function(arry1, arry2) {
-  const eqValues = [];
-  if (arry1.length === arry2.length) {
-    for (let i = 0; i < arry1.length; i++) {
-      if (arry1[i] === arry2[i]) {
-        eqValues.push(1);
-      }
-    }
-  }
-  if (eqValues.length === arry1.length) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = function(arry1, arry2) {
-  if (eqArrays(arry1, arry2)) {
-    console.log('✅✅✅ ' + 'Assertion Passed: Your Arrays are equal!');
-  
-  } else {
-    console.log('❌❌❌ ' + 'Assertion Failed: Your Arrays are not equal!');
-  }
-};
-
 const middle = function(array) {
   const arrayMiddle = [];
   if (array.length <= 2) {
@@ -40,21 +15,5 @@ const middle = function(array) {
   }
   return arrayMiddle;
 };
-  
-middle([1]); // => []
-middle([1, 2]); // => []
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-
-middle([1, 2, 3]); // => [2]
-middle([1, 2, 3, 4, 5]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
