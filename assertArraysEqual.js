d9522d22b0d6cf40b1204cf2b8fc780dd4522701
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-07 14:26:16 -0400

    remove test code and export function to modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(arry1, arry2) {
   if (eqArrays(arry1, arry2)) {
     console.log('✅✅✅ ' + 'Assertion Passed: Your Arrays are equal!');
@@ -7,24 +9,4 @@ const assertArraysEqual = function(arry1, arry2) {
   }
 };
 
-const eqArrays = function(arry1, arry2) {
-  const eqValues = [];
-  if (arry1.length === arry2.length) {
-    for (let i = 0; i < arry1.length; i++) {
-      if (arry1[i] === arry2[i]) {
-        eqValues.push(1);
-      }
-    }
-  }
-  if (eqValues.length === arry1.length) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = assertArraysEqual;
