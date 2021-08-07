64c56a336a51a07c2bb34820620b67b23af2f445
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-07 15:54:41 -0400

    refactor middleTest with deepEqual

--- test/middleTest.js
+++ test/middleTest.js
@@ -5,20 +5,20 @@ const assert = require('chai').assert;
 describe("#middle", function() {
   it('should return [] for inputs [1]', function() {
     let result = middle([1]);
-    assert.equal(eqArrays(result, []), true);
+    assert.deepEqual(result, []);
   });
   it('should return [2] for inputs [1,2,3]', function() {
-    assert.equal(eqArrays(middle([1,2,3]), [2]) , true);
+    assert.deepEqual(middle([1,2,3]), [2]);
   });
   it('should return [2,3] for [1,2,3,4]', function() {
-    assert.equal(eqArrays(middle([1,2,3,4]), [2,3]) , true);
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
   });
   it('should return middle element with string array', function() {
-    assert.equal(eqArrays(middle(["1","2","3"]), ["2"]) , true);
+    assert.deepEqual(middle(["1","2","3"]), ["2"]);
   });
   it("should not mutate the original array", function() {
     const arr = ["Loopy", "Lighthouse", "Labs"];
     middle(arr);
-    assert.equal(arr.length, 3);
+    assert.deepEqual(arr.length, 3);
   });
 });
