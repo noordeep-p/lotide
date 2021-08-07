cca48e0d0fc6b45f6f2dcef55f69ac6b47719c82
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-07 14:25:25 -0400

    create seperate file for test driver code

---
+++ test/middleTest.js
@@ -0,0 +1,20 @@
+const middle = require('./../assertArraysEqual');
+const assertArraysEqual = require('./../assertArraysEqual');
+
+middle([1]); // => []
+middle([1, 2]); // => []
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+
+middle([1, 2, 3]); // => [2]
+middle([1, 2, 3, 4, 5]); // => [3]
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+middle([1, 2, 3, 4]); // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
