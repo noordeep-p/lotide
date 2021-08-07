91a336c096415a9b23d726adcf12ff22642dd997
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-07 13:58:54 -0400

    remove and seperate driver code/ export to modules

--- head.js
+++ head.js
@@ -1,11 +1,5 @@
-// assertEqual Function
-const assertEqual = require('./assertEqual');
-
-// head Function
 const head = function(input) {
   return input[0];
 };
 
-// Test Case 1: Check the returned array elements
-assertEqual(head([1]), 1);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
