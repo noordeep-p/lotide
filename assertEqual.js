8fb1455418bfb664d6992b06bbb9de9004de6e0e
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-07 13:52:21 -0400

    implement export of function

--- assertEqual.js
+++ assertEqual.js
@@ -8,7 +8,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1, 3);
+module.exports = assertEqual;
