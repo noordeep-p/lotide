d7b062a137d3159807b536912b7f28290063dde2
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-08-07 15:41:05 -0400

    refactor tailTest with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,24 @@
+const eqArrays = require("../eqArrays");
 const tail = require('./../tail');
-const assertEqual = require('./../assertEqual');
+const assert = require('chai').assert;
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: An array with only one element and empty array should return a empty array
-const result2 = tail([]);
-const result3 = tail([1]);
-assertEqual(result2.length, 0); // ensure the lenght of array is 0
-assertEqual(Array.isArray(result2), true); // ensure a array is returned
-assertEqual(result3.length, 0); // ensure the lenght of array is 0
-assertEqual(Array.isArray(result3), true); // ensure a array is returned
-
-// Test Case 3: Check the original array
-const words = ["Loopy", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", function() {
+  it('should return [2,3] for inputs [1,2,3]', function() {
+    let result = tail([1, 2, 3]);
+    assert.equal(eqArrays(result, [2, 3]), true);
+  });
+  it('should return ["my name is", "noor"] for inputs ["hi","my name is","noor"]', function() {
+    assert.equal(eqArrays(tail(["hi","my name is","noor"]), ["my name is", "noor"]) , true);
+  });
+  it('should return ["2", 3] for ["1", "2", 3]', function() {
+    assert.equal(eqArrays(tail(["1", "2", 3]), ["2", 3]) , true);
+  });
+  it('should return An array with only one element and empty array should return a empty array', function() {
+    assert.equal(eqArrays(tail(["1"]), []) , true);
+  });
+  it("should not mutate the original array", function() {
+    const arr = ["Loopy", "Lighthouse", "Labs"];
+    tail(arr);
+    assert.equal(arr.length, 3);
+  });
+});
